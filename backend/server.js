2b94c0d2e232668c3efd42ebcc5f69bf33bfb6f4
Minh <minhnnse161013@fpt.edu.vn>
2025-10-06 14:32:07 +0700

    add config/db.js to connect moongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import taskRoutes from './src/routes/taskRoutes.js';
+import { connect } from 'mongoose';
+import { connectDB } from './src/config/db.js';
+import 'dotenv/config';
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use('/api/task', taskRoutes);
 
-app.listen(3000, () => {
-    console.log('Server đã chạy trên port 3000');
+app.listen(PORT, () => {
+    console.log('Server đã chạy trên port ' + PORT);
 });
 

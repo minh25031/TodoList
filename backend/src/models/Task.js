8100b3a7ef1f5c9da3b19f2ddd668c3a7e2dcd45
Minh <minhnnse161013@fpt.edu.vn>
2025-10-06 14:40:48 +0700

    add model Task

---
+++ backend/src/models/Task.js
@@ -0,0 +1,26 @@
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+    },
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: 'active',
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    completedAt: {
+        type: Date,
+        default: null,
+    },
+}, {
+    timestamps: true,
+});
+
+const Task = mongoose.model('Task', taskSchema);
+export default Task;

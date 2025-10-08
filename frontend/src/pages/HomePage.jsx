40a250358e5847cfd85ae048258df00e54d052a4
Minh <minhnnse161013@fpt.edu.vn>
2025-10-08 20:44:45 +0700

    set up frontend

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Home Page</h1>
+            <p>This is the home page content</p>
+        </div>
+    );
+};
+
+export default HomePage;

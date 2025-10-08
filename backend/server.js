236f4a2830b6246e164a046702141f95dc648999
Minh <minhnnse161013@fpt.edu.vn>
2025-10-08 20:25:24 +0700

    thay đổi kết nối db trước rồi mới chạy server

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ import 'dotenv/config';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// Kết nối DB
-connectDB();
 
 // Middleware
 app.use(express.json());
@@ -16,10 +14,18 @@ app.use(express.json());
 // Routes
 app.use('/api/task', taskRoutes);
 
+// Kết nối DB
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server đã chạy trên port ' + PORT);
+        console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
+    });
+})
+    .catch((err) => {
+        console.error('Kết nối DB thất bại:', err);
+        process.exit(1);
+    });
+
+
 // Swagger UI route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.listen(PORT, () => {
-    console.log('Server đã chạy trên port ' + PORT);
-    console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
-});

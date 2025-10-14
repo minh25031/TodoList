572fd66d19b8760f32634d087665ac88b9b5fda3
Minh <minhnnse161013@fpt.edu.vn>
2025-10-14 16:51:37 +0700

    add UI Header and AddTask

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='space-2 text-center '>
+            <h1 className='text-4xl font-bold text-transparent bg-primary bg-clip-text'>To Do List</h1>
+
+            <p className='text-muted-foreground'>Lo làm đi</p>
+        </div>
     )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
-import Headers from '@/components/Headers';
+import Header from '../components/Header';
+import AddTask from '@/components/AddTask';
+import TaskList from '@/components/TaskList';
+import StatsAndFillters from '@/components/StatsAndFillters';
+import TaskListAndPagination from '@/components/TaskListAndPagination';
+import DateTimeFilter from '@/components/DateTimeFilter';
+import Footer from '@/components/Footer';
+
 
 const HomePage = () => {
     return (
-        <div className='container pt-8 mx-auto'>
-            <div className='w-full max-w-2xl mx-auto space-y-6'>
+        <div className="min-h-screen w-full bg-white relative">
+            {/* Amber Glow Background */}
+            <div
+                className="absolute inset-0 z-0"
+                style={{
+                    backgroundImage: `
+        radial-gradient(125% 125% at 50% 90%, #ffffff 40%, #f59e0b 100%)
+      `,
+                    backgroundSize: "100% 100%",
+                }}
+            />
+            <div className='container pt-8 mx-auto relative z-10'>
+                <div className='w-full max-w-2xl mx-auto p-6 space-y-6'>
 
-                {/* Đầu Trang */}
-                <Headers />
-            </div>
-        </div>
+                    {/* Đầu Trang */}
+                    <Header />
+
+                    {/* Tạo nhiệm vụ */}
+                    <AddTask />
+
+                    {/* Thống kê và bộ lọc */}
+                    <StatsAndFillters />
+
+                    {/* Danh sách nhiệm vụ */}
+                    <TaskList />
+
+                    {/* Phân trang và lọc theo ngày */}
+                    <div className='flex flex-col items-center justify-between gap-6 sm:flex-row'>
+                        <TaskListAndPagination />
+                        <DateTimeFilter />
+                    </div>
+
+                    {/* Chân trang */}
+                    <Footer />
+
+                </div>
+            </div></div>
+
+
     );
 };
 

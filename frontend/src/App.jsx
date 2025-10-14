12b127c445dc67c1076b047b8f67c84e07427e06
Minh <minhnnse161013@fpt.edu.vn>
2025-10-14 15:27:41 +0700

    instaill taldwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Toaster, toast } from 'sonner';
+import { Toaster } from 'sonner';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
